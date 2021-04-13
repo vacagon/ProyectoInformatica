dcb17bfc9267ba5ec5f3abf524d8cf0e9f0bd538
JavierLubilloFisac <81471923+JavierLubilloFisac@users.noreply.github.com>
2021-04-13 18:31:37 +0200

    Update Proyecto/CreditCard.cpp
    Co-authored-by: vacagon <66060125+vacagon@users.noreply.github.com>

--- Proyecto/CreditCard.cpp
+++ Proyecto/CreditCard.cpp
@@ -25,17 +25,10 @@ const string& CreditCard::getCardholder() const {
 }
 
 const string CreditCard::show() const {
-    string output, id_aux, num_aux, ba_aux;
-    stringstream ss_id, ss_num, ss_ba;
-    ss_id << id ;
-    ss_id >> id_aux;
-    ss_num << number ;
-    ss_num >> num_aux;
-    ss_ba << billing_address ;
-    ss_ba >> ba_aux;
-    output = "\t" + id_aux;
-    output += " - Credit Card:\t" + ba_aux;
-    output += "\t" + num_aux;
-    output += " - " + cardholder;
-    return output;
+    stringstream ss;
+    ss << "\tid " << id << " - Credit Card:" << endl
+         << "\tBilling address: " << billing_address->show() << endl
+         << "\t" << number << " - "
+         << cardholder << endl;
+    return ss.str();
 }
