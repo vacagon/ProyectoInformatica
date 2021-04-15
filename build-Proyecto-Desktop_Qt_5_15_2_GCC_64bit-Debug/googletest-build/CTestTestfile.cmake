80d352dfc0c71e5f5672260ecb3e5b34e5925281
Gonzalo Vaca <gonzalovaca99@gmail.com>
2021-04-15 13:05:04 +0200

    Eliminado la carpeta de build por defecto que se te había creado

--- build-Proyecto-Desktop_Qt_5_15_2_GCC_64bit-Debug/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/javier/ProyectoInformatica/build-Proyecto-Desktop_Qt_5_15_2_GCC_64bit-Debug/googletest-src
-# Build directory: /home/javier/ProyectoInformatica/build-Proyecto-Desktop_Qt_5_15_2_GCC_64bit-Debug/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build-Proyecto-Desktop_Qt_5_15_2_GCC_64bit-Debug/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/javier/ProyectoInformatica/build-Proyecto-Desktop_Qt_5_15_2_GCC_64bit-Debug/googletest-src/googletest
-# Build directory: /home/javier/ProyectoInformatica/build-Proyecto-Desktop_Qt_5_15_2_GCC_64bit-Debug/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
