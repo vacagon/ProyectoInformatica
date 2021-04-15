90f9e7a637434ff6cf5e2341d9cdd25b7b21bd97
Gonzalo Vaca <gonzalovaca99@gmail.com>
2021-04-15 13:57:58 +0200

    Ultimos retoques a la clase order

--- Proyecto/Order.cpp
+++ Proyecto/Order.cpp
@@ -1,7 +1,7 @@
 #include <ctime>
 #include "Order.hpp"
 
-Order::Order(const unsigned long &reference, const vector<unsigned long>& products, const int address, const int payment_method, const float total) {
+Order::Order(const unsigned long &reference, const vector<unsigned long>& products, const int &address, const int &payment_method, const float &total) {
     setReference(reference);
     for (const unsigned long& product : products) {
         addProduct(product);
@@ -12,7 +12,7 @@ Order::Order(const unsigned long &reference, const vector<unsigned long>& produc
     date = time(0);
 }
 
-Order::Order(const unsigned long &reference, const int address, const int payment_method) {
+Order::Order(const unsigned long &reference, const int& address, const int& payment_method) {
     setReference(reference);
     setDeliveryAddress(address);
     setPaymentMethod(payment_method);
@@ -25,46 +25,46 @@ Order::Order() {
 
 Order::~Order() {}
 
-void Order::setReference (unsigned long ref) {
+void Order::setReference (const unsigned long &ref) {
     reference = ref;
 }
 
-unsigned long Order::getReference() const {
+const unsigned long &Order::getReference() const {
     return reference;
 }
 
-vector<unsigned long> Order::getProducts() const {
+const vector<unsigned long> &Order::getProducts() const {
     return products;
 }
 
-void Order::addProduct(unsigned long ref) {
+void Order::addProduct(const unsigned long &ref) {
     reference = ref;
 }
 
-time_t Order::getDate() const {
+const time_t &Order::getDate() const {
     return date;
 }
 
-int Order::getDeliveryAddress() const {
+const int &Order::getDeliveryAddress() const {
     return delivery_address;
 }
 
-void Order::setDeliveryAddress(int ad) {
+void Order::setDeliveryAddress(const int &ad) {
     delivery_address = ad;
 }
 
-int Order::getPaymentMethod() const {
+const int& Order::getPaymentMethod() const {
     return payment_method;
 }
 
-void Order::setPaymentMethod(int pm) {
+void Order::setPaymentMethod(const int &pm) {
     payment_method = pm;
 }
 
-float Order::getTotal() const {
+const float& Order::getTotal() const {
     return total;
 }
 
-void Order::setTotal(float t) {
+void Order::setTotal(const float &t) {
     total = t;
 }
