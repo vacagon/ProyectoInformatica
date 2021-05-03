a608a2c645bd39a4c88eb3034ca59b81db87a695
Gonzalo Vaca <gonzalovaca99@gmail.com>
2021-05-03 13:02:23 +0200

    .

--- Proyecto/Build/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gonzalo/ProyectoInformatica/Proyecto/Build/googletest-src/googletest
-# Build directory: /home/gonzalo/ProyectoInformatica/Proyecto/Build/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
